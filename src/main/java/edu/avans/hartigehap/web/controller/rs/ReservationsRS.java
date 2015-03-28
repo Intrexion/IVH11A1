2d231fea7631e6b70fbdb2166bde76cad34ade87
Dennis123789 <dennis_vd_munckhof@live.nl>
2015-03-28 14:52:04 +0100

    Restful Reservation controller improvements
    Cleanup, improvements, formatting and implement reservationcode generator.

--- src/main/java/edu/avans/hartigehap/web/controller/rs/ReservationsRS.java
+++ src/main/java/edu/avans/hartigehap/web/controller/rs/ReservationsRS.java
@@ -16,8 +16,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.View;
 
 import edu.avans.hartigehap.domain.DiningTable;
 import edu.avans.hartigehap.domain.Reservation;
@@ -28,7 +26,7 @@ import edu.avans.hartigehap.web.controller.ReservationController;
 
 @Controller
 public class ReservationsRS {
-	private final Logger logger = LoggerFactory.getLogger(RestaurantsRS.class);
+	private final Logger logger = LoggerFactory.getLogger(ReservationsRS.class);
 
 
 	@Autowired
@@ -36,11 +34,6 @@ public class ReservationsRS {
 	
 	@Autowired
 	private RestaurantService restaurantService;
-	
-	private View jsonView = null;
-
-	private static final String DATA_FIELD = "data";
-	private static final String ERROR_FIELD = "error";
 	
 	/**
 	 * list all restaurants.
@@ -61,18 +54,21 @@ public class ReservationsRS {
 		
 		Restaurant restaurant = restaurantService.findById(reservation.getRestaurant().getId());
 		DiningTable diningTable = ReservationController.checkReservation(reservation, (List<DiningTable>) restaurant.getDiningTablesBySeats(reservation.getCustomer().getPartySize()));
-		if(diningTable == null){
+
+		if (diningTable == null) {
 			response.put("result", "FAIL");
-		}else{
+		} else {
 			reservation.setRestaurant(restaurant);
 			restaurant.getReservations().add(reservation);
 			reservation.setDiningTable(diningTable);
 			diningTable.getReservations().add(reservation);
 			reservation.getCustomer().setReservation(reservation);
+			reservation.setCode(ReservationController.randomGenerator());
 			
 			reservation = reservationService.save(reservation);
 			response.put("result", "OK");
 		}
+
 		response.put("reservation", reservation);
 		return response;
 	}
@@ -80,9 +76,7 @@ public class ReservationsRS {
 	@RequestMapping(value = RSConstants.URL_PREFIX + "/reservations/{reservationId}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
 	@ResponseBody
 	public void delete(@PathVariable Long reservationId, HttpServletResponse httpResponse, WebRequest httpRequest) {
-		
 		Reservation toBeDeleted = reservationService.findById(reservationId);
-		
 		logger.debug("reservationId: {}", toBeDeleted);
 		reservationService.delete(toBeDeleted);
 	}
@@ -100,17 +94,8 @@ public class ReservationsRS {
 		logger.debug("reservationId: {}", reservationId);
 		return reservationService.findById(reservationId);
 	}
-	
-	private ModelAndView createErrorResponse(String sMessage) {
-		return new ModelAndView(jsonView, ERROR_FIELD, sMessage);
-	}
 
 	public void setRestaurantService(ReservationService reservationService) {
 		this.reservationService = reservationService;
 	}
-
-	public void setJsonView(View jsonView) {
-		this.jsonView = jsonView;
-	}
-	
 }
