395c7580452fa16aed4b8761afa448b60c388f79
BaasT <tom_jaspers@hotmail.com>
2015-03-30 13:11:23 +0200

    PUT (via een Post)
    Lelijke oplossing wachten op erco's oplossing

--- src/main/java/edu/avans/hartigehap/web/controller/rs/ReservationsRS.java
+++ src/main/java/edu/avans/hartigehap/web/controller/rs/ReservationsRS.java
@@ -11,7 +11,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -19,9 +21,11 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.View;
 
+import edu.avans.hartigehap.domain.Customer;
 import edu.avans.hartigehap.domain.DiningTable;
 import edu.avans.hartigehap.domain.Reservation;
 import edu.avans.hartigehap.domain.Restaurant;
+import edu.avans.hartigehap.service.CustomerService;
 import edu.avans.hartigehap.service.ReservationService;
 import edu.avans.hartigehap.service.RestaurantService;
 import edu.avans.hartigehap.web.controller.ReservationController;
@@ -37,6 +41,9 @@ public class ReservationsRS {
 	@Autowired
 	private RestaurantService restaurantService;
 	
+	@Autowired
+	private CustomerService customerService;
+	
 	private View jsonView = null;
 
 	private static final String DATA_FIELD = "data";
@@ -44,7 +51,6 @@ public class ReservationsRS {
 	
 	/**
 	 * list all restaurants.
-	 * 
 	 * @return
 	 */
 	@RequestMapping(value = RSConstants.URL_PREFIX + "/reservations", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
@@ -87,11 +93,46 @@ public class ReservationsRS {
 		reservationService.delete(toBeDeleted);
 	}
 	
-	@RequestMapping(value = RSConstants.URL_PREFIX + "/reservations/{reservationId}", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
+	@RequestMapping(value = RSConstants.URL_PREFIX + "/reservations/{reservationId}", 
+			method = RequestMethod.POST,
+			produces = MediaType.APPLICATION_JSON_VALUE)
 	@ResponseBody
-	public void update(@PathVariable Reservation reservation, HttpServletResponse httpResponse, WebRequest httpRequest) {
-		logger.debug("reservationId: {}", reservation);
-		reservationService.save(reservation);
+	public HashMap<String, Object> updateReservationJson(@PathVariable Long reservationId, Reservation updateReservation, BindingResult bindingResult) {
+		HashMap<String, Object> response = new HashMap<String, Object>();
+		Reservation reservation = reservationService.findById(updateReservation.getId());
+		
+		reservation.setDescription(updateReservation.getDescription());
+		reservation.setStartDate(updateReservation.getStartDate());
+		reservation.setEndDate(updateReservation.getEndDate());
+		
+		Customer cust = reservation.getCustomer();
+		Customer upCust = updateReservation.getCustomer();
+		cust.setFirstName(upCust.getFirstName());
+		cust.setLastName(upCust.getLastName());
+		cust.setPhone(upCust.getPhone());
+		cust.setEmail(upCust.getEmail());
+		cust.setPartySize(upCust.getPartySize());
+		
+		
+		DiningTable diningTable = ReservationController.checkReservation(updateReservation, 
+				(List<DiningTable>) reservation.getRestaurant().getDiningTablesBySeats(reservation.getCustomer().getPartySize()));
+		if(diningTable == null){
+			response.put("result", "FAIL");
+		}else{
+			DiningTable removeDiningTable = reservation.getDiningTable();
+			if(diningTable.getId() != removeDiningTable.getId()){
+				removeDiningTable.getReservations().remove(reservation);
+				reservation.setDiningTable(diningTable);
+				diningTable.getReservations().add(reservation);
+			}
+			reservation.getCustomer().setReservation(reservation);
+			
+			reservation = reservationService.save(reservation);
+			response.put("result", "OK");
+		}
+		response.put("reservation", reservation);
+		
+		return response;
 	}	
 	
 	@RequestMapping(value = RSConstants.URL_PREFIX + "/reservations/{reservationId}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
